ade734c5357d5cdebc42f55ae6bf459fa35a7566
vivekanand3435 <vivekanand3435@gmail.com>
2018-10-06 10:01:24 +0530

    Create Build_Lowest_Number_by_Removing_n_digits.cpp

---
+++ Build_Lowest_Number_by_Removing_n_digits.cpp
@@ -0,0 +1,39 @@
+// C++ program to build the smallest number by removing 
+// n digits from a given number 
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+string buildLowestNumber(string num, int k) 
+{ 
+        int count = k;
+        vector<char> str(num.begin(), num.end()) ;
+        int i = 0;
+        int len = num.length();
+        vector<char>::iterator itr = str.begin();
+        while(count>0)
+        {
+            while( itr+1 != str.end() && *itr <= *(itr+1))
+            {
+                itr++;
+            }
+            str.erase(itr);
+            itr--;
+            count--;
+        }
+     
+        string temp(str.begin(), str.end());
+        //cout<<endl<<temp<<endl;
+        return temp;
+    
+} 
+
+// Driver program to test above function 
+int main() 
+{ 
+	string str = "5555782"; 
+	int n = 3; 
+	cout << buildLowestNumber(str, n)<< endl; 
+	return 0; 
+} 
+// This code is contributed by Gaurav Mamgain 
